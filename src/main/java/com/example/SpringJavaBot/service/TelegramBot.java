4614b7e7907251dd871ff2de8848c591b3148d16
pogorelov-evgen <72943262+pogorelov-evgen@users.noreply.github.com>
2023-10-17 19:31:45 +0300

    test

--- src/main/java/com/example/SpringJavaBot/service/TelegramBot.java
+++ src/main/java/com/example/SpringJavaBot/service/TelegramBot.java
@@ -3,12 +3,14 @@ package com.example.SpringJavaBot.service;
 
 import com.example.SpringJavaBot.config.BotConfig;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     final BotConfig config;
@@ -39,6 +41,9 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case ("/start"):
                            startCommandReceived(chatId,update.getMessage().getChat().getFirstName());
                             break;
+                case("/new"): sendMessage(chatId,"Sorry");
+                    break;
+
                 default: sendMessage(chatId,"Sorry, command was not recognized");
 
             }
@@ -49,6 +54,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
 
         String answer = "Hi, " + name + ", nice to meet you!";
+        log.info("Info about: " + name);
 
         sendMessage(chatId,answer);
 
@@ -62,7 +68,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             execute(message);
         }
         catch (TelegramApiException e){
-
+            throw new RuntimeException(e);
         }
     }
 }
